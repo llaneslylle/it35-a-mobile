6ac2aa69f57622b5be97f6b7f90b0b87954c8b30
llaneslylle <llaneslylle@gmail.com>
2026-03-30 09:58:11 +0800

    Change the component directory

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/template';
+import ExploreContainer from '../components/ExploreContainer copy';
 import './Home.css';
 
 const Home: React.FC = () => {
